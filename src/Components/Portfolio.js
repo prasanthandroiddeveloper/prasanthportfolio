dd50a420023b76c76700ed8b0c496b4a85da2935
PrasanthAndroiddeveloper <githubprasanth@gmail.com>
2020-09-25 19:31:53 +0530

    first commit

---
+++ src/Components/Portfolio.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class Portfolio extends Component {
+  render() {
+
+    if(this.props.data){
+      var projects = this.props.data.projects.map(function(projects){
+        var projectImage = 'images/portfolio/'+projects.image;
+        return <div key={projects.title} className="columns portfolio-item">
+           <div className="item-wrap">
+            <a href={projects.url} title={projects.title}>
+               <img />
+               <div>
+                  <div className="portfolio-item-meta">
+                 <h5 style={{height:"50px",color:"black"}}>{projects.title}</h5>
+                     <b><h5 style={{color:"grey"}}>{projects.category}</h5></b>
+                  </div>
+                </div>
+              <div className="link-icon"><i className="fa fa-link"></i></div>
+            </a>
+          </div>
+        </div>
+      })
+    }
+
+    return (
+      <section id="portfolio">
+
+      <div className="row">
+
+         <div className="twelve columns collapsed">
+
+            <h1>Check Out Some of My Works.</h1>
+
+            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+                {projects}
+            </div>
+          </div>
+      </div>
+   </section>
+    );
+  }
+}
+
+export default Portfolio;
